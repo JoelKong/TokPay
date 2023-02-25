86545acb2d777cec45f62a7da9fb85ae94cb8075
Joel Kong <joelkong2001@gmail.com>
2023-02-25 15:56:53 +0800

    feat: added description boilerplate

--- tokpay/pages/index.js
+++ tokpay/pages/index.js
@@ -1,7 +1,20 @@
+import dynamic from "next/dynamic";
+const DynamicDescription = dynamic(
+  () => import("../components/description/description"),
+  {
+    loading: () => <p>Loading...</p>,
+  }
+);
 import Introduction from "../components/introduction/intro";
+import { samplePieData } from "../utils/charts/samplechart";
 
 function Landing() {
-  return <Introduction />;
+  return (
+    <main>
+      <Introduction />
+      <DynamicDescription samplePieData={samplePieData} />
+    </main>
+  );
 }
 
 export default Landing;
