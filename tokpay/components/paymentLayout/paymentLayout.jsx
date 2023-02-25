2181042b18f7d7f5c69206853e36b76b5dd104d2
whysilon <valencino1306@gmail.com>
2023-02-25 23:31:50 +0800

    feat: Payment UI

--- tokpay/components/paymentLayout/paymentLayout.jsx
+++ tokpay/components/paymentLayout/paymentLayout.jsx
@@ -1,11 +1,12 @@
 import SideBar from "../sidebar/sidebar";
 import classes from "./paymentLayout.module.css"
+import Pay from "../payment/pay.jsx"
 
 function PaymentLayout() {
-
     return (
         <div className = {classes.body}>
             <SideBar/>
+            <Pay/>
         </div>
     )
 }
