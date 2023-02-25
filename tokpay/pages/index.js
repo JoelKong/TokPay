856060e82aeddf5aa6772e6b73a2fdfd0cf4fc4d
Joel Kong <joelkong2001@gmail.com>
2023-02-25 14:45:20 +0800

    feat: set up boilerplate for description and added sample chart

--- tokpay/pages/index.js
+++ tokpay/pages/index.js
@@ -1,7 +1,21 @@
+import { Fragment } from "react";
+import dynamic from "next/dynamic";
+import { samplePieData } from "../utils/charts/samplechart";
+const DynamicDescription = dynamic(
+  () => import("../components/description/description.jsx"),
+  {
+    loading: () => <p>Loading...</p>,
+  }
+);
 import Introduction from "../components/introduction/intro";
 
 function Landing() {
-  return <Introduction />;
+  return (
+    <Fragment>
+      <Introduction />
+      <DynamicDescription samplePieData={samplePieData} />
+    </Fragment>
+  );
 }
 
 export default Landing;
